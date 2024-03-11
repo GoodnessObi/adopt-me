3eccca223d643cd16a752e0ed5330a391730a3d1
GoodnessObi <goodnessobi@gmail.com>
2024-03-11 23:18:13 +0100

    chore: fix deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./pages/SearchParams";
 import Details from "./pages/Details";
+import './style.css'
 
 // import AdoptedPetContext from "./context/AdoptedPet";
 import { Pet } from "./types/APIResponsesTypes";
